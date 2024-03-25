f1afef723919395afb2b1be759e8a69023d0a5c9
Furkan Doğu <furkandogu2018@gmail.com>
2024-03-25 20:32:21 +0300

    bitti

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
 import CourseInput from "./components/CourseInput";
 
 export default function App() {
   const [modalOpen, setModalOpen] = useState(false);
+  const [courses, setCourses] = useState([]);
 
   const startModal = () => {
     setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const onAddCourse = (courseTitle) => {
+    setCourses((currentCourses) => [
+      ...currentCourses,
+      { text: courseTitle, id: new Date().getTime().toString() },
+    ]);
+    closeModal();
   };
 
   return (
@@ -15,7 +28,18 @@ export default function App() {
       <StatusBar style="light" />
       <View style={styles.container}>
         <Button title="Kurs Ekle" color={"red"} onPress={startModal} />
-        <CourseInput modalOpen={modalOpen} />
+        <CourseInput
+          modalOpen={modalOpen}
+          onAddCourse={onAddCourse}
+          closeModal={closeModal}
+        />
+        <View>
+          <FlatList data={courses} renderItem={({item}) => (
+            <View key={item.id} style={styles.courseItem}>
+              <Text style={styles.courseText}>{item.text}</Text>
+            </View>
+          )} />
+        </View>
       </View>
     </>
   );
@@ -28,4 +52,13 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 20,
   },
+  courseItem: {
+    backgroundColor: "gray",
+    margin: 8,
+    borderRadius: 5,
+  },
+  courseText: {
+    padding: 8,
+    color: "white",
+  }
 });
--- components/CourseInput.jsx
+++ components/CourseInput.jsx
@@ -1,19 +1,46 @@
-import { Image, Modal, StyleSheet, Text, View, TextInput, Button } from "react-native";
-import React from "react";
+import {
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+} from "react-native";
+import React, { useState } from "react";
 
-export default function CourseInput({ modalOpen }) {
+export default function CourseInput({ modalOpen, onAddCourse, closeModal }) {
+  const [enteredCourseText, setEnteredCourseText] = useState("");
+
+  const addCourseHandler = () => {
+    onAddCourse(enteredCourseText);
+    setEnteredCourseText("")
+  };
+
   return (
     <View>
       <Modal animationType="slide" visible={modalOpen}>
         <View style={styles.inputContainer}>
-            <Image style={styles.image} source={require("../assets/images/furkan-logo.png")} />
-            <TextInput style={styles.textInput} placeholder="Yeni Kurs Ekle" />
+          <Image
+            style={styles.image}
+            source={require("../assets/images/furkan-logo.png")}
+          />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Yeni Kurs Ekle"
+            value={enteredCourseText}
+            onChangeText={(text) => setEnteredCourseText(text)}
+          />
           <View style={styles.buttonContainer}>
             <View style={styles.button}>
-                <Button title="İptal Et" color={"red"} />
+              <Button
+                title="İptal Et"
+                color={"red"}
+                onPress={closeModal}
+              />
             </View>
             <View style={styles.button}>
-                <Button title="Ekle" color={"blue"} />
+              <Button title="Ekle" color={"blue"} onPress={addCourseHandler} />
             </View>
           </View>
         </View>
@@ -23,32 +50,32 @@ export default function CourseInput({ modalOpen }) {
 }
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "gray",
-        padding: 15,
-    },
-    image: {
-        width: 150,
-        height: 150,
-        margin: 20
-    },
-    textInput: {
-        borderWidth: 1,
-        width: "100%",
-        padding: 10,
-        borderRadius: 10,
-        backgroundColor: "pink",
-        borderColor: "pink",
-    },
-    buttonContainer: {
-        flexDirection: "row",
-        marginTop: 15,
-    },
-    button: {
-        width: 100,
-        marginHorizontal: 8,
-    }
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "gray",
+    padding: 15,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    margin: 20,
+  },
+  textInput: {
+    borderWidth: 1,
+    width: "100%",
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "pink",
+    borderColor: "pink",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginTop: 15,
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
 });
